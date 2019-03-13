95e75b8b7470b3cf2c06750affbf3eecfc3354b3
25040呂紹謙 <43640574+kenlu250@users.noreply.github.com>
2019-03-13 22:01:06 +0800

    Create ns001_基礎.cpp

---
+++ ns001_基礎.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+  stack<int> book;
+  int n, m, times;
+  int t = 1;
+  int take = 0;
+  int num[n];
+…        c++;
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++){
+    cout << num[i] << " ";
+  }
+}
