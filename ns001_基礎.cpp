4524ba6dba2b4846ea0697431d5d90f4a0889759
25040呂紹謙 <43640574+kenlu250@users.noreply.github.com>
2019-03-13 22:03:34 +0800

    Update ns001_基礎.cpp

--- ns001_基礎.cpp
+++ ns001_基礎.cpp
@@ -8,7 +8,27 @@ int main() {
   int t = 1;
   int take = 0;
   int num[n];
-…        c++;
+  char d;
+
+  cin >> n >> m; // n = 書的數量， m = 移動次數 
+
+  for (int i = 0; i < m; i++){
+    int c = 0;
+
+    cin >> d >> times; // d = 動作， time = 拿的個數 
+
+    if (d == 'a'){ // a為疊上書本
+      for (int k = 0; k < times; k++){
+        book.push(t);
+        t += 1;
+      }
+    }
+    else if (d == 'b'){ // b為拿起書本
+      while (c < times){
+        num[take] = book.top();
+        book.pop();
+        take++;
+        c++;
       }
     }
   }
